6ae096dba8acc1587d2679d582167a0c6345bbc1
Gabriel Jabes <gabriel.jabesldl@gmail.com>
2026-04-18 21:12:48 -0300

    ajustes nas perturbacoes

--- MLP/src/heuristics/perturbacao.cpp
+++ MLP/src/heuristics/perturbacao.cpp
@@ -5,10 +5,10 @@
 Solucao Perturbacao(Solucao& s, Data& problem_info, vector<vector<Subsequence>>& matrix){
     Solucao s_linha = s;
     
-    size_t max = (size_t)ceil(problem_info.getDimension()/10.0);
+    size_t max_tamanho = max((size_t)2, (size_t)ceil(problem_info.getDimension()/10.0));
 
-    size_t A = rand() % (max - 1) + 2;
-    size_t B = rand() % (max - 1) + 2;
+    size_t A = rand() % (max_tamanho - 1) + 2;
+    size_t B = rand() % (max_tamanho - 1) + 2;
 
      size_t total_gaps = problem_info.getDimension() - A - B - 1;
      size_t gap1 = rand() % (total_gaps - 1);
@@ -31,8 +31,8 @@ Solucao Perturbacao(Solucao& s, Data& problem_info, vector<vector<Subsequence>>&
     s_linha.sequencia.insert(s_linha.sequencia.begin() + p_A, bloco_b.begin(), bloco_b.end());
     s_linha.sequencia.insert(s_linha.sequencia.begin() + (p_B - A + B), bloco_a.begin(), bloco_a.end());
 
-    UpdateAllSubseq(s, matrix, problem_info);
-    s.valorObj = matrix[0][s.sequencia.size()-1].C;
+    UpdateAllSubseq(s_linha, matrix, problem_info);
+    s_linha.valorObj = matrix[0][s.sequencia.size()-1].C;
 
     return s_linha;
 }
