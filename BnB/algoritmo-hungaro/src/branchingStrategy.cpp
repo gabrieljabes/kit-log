d28d9af5665844a187b597f880ef6da4da9c4d02
Gabriel Jabes <gabriel.jabesldl@gmail.com>
2026-04-23 19:59:59 -0300

    BBS strategy

--- BnB/algoritmo-hungaro/src/branchingStrategy.cpp
+++ BnB/algoritmo-hungaro/src/branchingStrategy.cpp
@@ -1,4 +1,5 @@
 #include "branchingStrategy.h"
+#include <queue>
 
 Node strategyBFS(list<Node>& tree){
     Node next_node = tree.front();
@@ -6,9 +7,24 @@ Node strategyBFS(list<Node>& tree){
 
     return next_node;
 }
+
 Node strategyDFS(list <Node>& tree){
     Node next_node = tree.back();
     tree.pop_back();
 
     return next_node;
+}
+
+Node strategyBBS(priority_queue<Node>& tree_BBS){
+    Node next_node = tree_BBS.top();
+    tree_BBS.pop();
+
+    return next_node;
+}
+
+Node DFS_or_BFS(string strategy, list<Node>& tree){
+    if(strategy == "BFS")
+        return strategyBFS(tree);
+    if(strategy == "DFS")
+        return strategyDFS(tree);
 }
