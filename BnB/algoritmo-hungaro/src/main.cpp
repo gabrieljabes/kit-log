0abe8b2381094b4c062000427642dcd6d1e3f1e3
Gabriel Jabes <gabriel.jabesldl@gmail.com>
2026-04-23 20:02:04 -0300

    implementação

--- BnB/algoritmo-hungaro/src/main.cpp
+++ BnB/algoritmo-hungaro/src/main.cpp
@@ -10,7 +10,10 @@ using namespace std;
 #include <algorithm>
 #include <climits>
 #include <list>
+#include <queue>
 
+void runBBS(Data& problem_info, Node& root, vector<int>& best_s, double& upper_bound);
+void runDFS_BFS(Data& problem_info, Node& root, vector<int>& best_s, double& upper_bound, string& strategy);
 
 int main(int argc, char** argv) {
 
@@ -22,14 +25,31 @@ int main(int argc, char** argv) {
 	Node root;
 	updateNode(&root, data);
 
-	list<Node> tree;
-	tree.push_back(root);
-
 	double upper_bound = stod(argv[2]) + 1;
 	vector <int> best_s;
+	string strategy = argv[3];
 
+	if(strategy == "BBS")
+		runBBS(data, root, best_s, upper_bound);
+	else{
+		runDFS_BFS(data, root, best_s, upper_bound, strategy);
+	}
+
+	
+
+	for(auto& i : best_s)
+		cout << i << ", ";
+	cout << endl;
+
+
+	return 0;
+}
+
+void runBBS(Data& problem_info, Node& root, vector<int>& best_s, double& upper_bound){
+	priority_queue<Node> tree;
+	tree.push(root);
 	while(!tree.empty()){
-		auto node = strategyDFS(tree);
+		auto node = strategyBBS(tree);
 
 		if(node.feasible){
 			if(node.lower_bound < upper_bound){
@@ -40,7 +60,7 @@ int main(int argc, char** argv) {
 		}
 	
 		for(int i = 0; i < node.subtours[node.choosen].size() - 1; i++){
-			Node n;
+			Node n;	
 			n.forbidden_arcs = node.forbidden_arcs;
 
 			pair<int, int> forbidden_arc;
@@ -49,16 +69,42 @@ int main(int argc, char** argv) {
 			forbidden_arc.second = node.subtours[node.choosen][i+1] - 1;
 
 			n.forbidden_arcs.push_back(forbidden_arc);
-			updateNode(&n, data);
+			updateNode(&n, problem_info);
+			if(n.lower_bound <= upper_bound)
+				tree.push(n);	
+		}
+	}
+
+}
+
+void runDFS_BFS(Data& problem_info, Node& root, vector<int>& best_s, double& upper_bound, string& strategy){
+	list<Node> tree;
+	tree.push_back(root);
+	while(!tree.empty()){
+		auto node = DFS_or_BFS(strategy, tree);
+
+		if(node.feasible){
+			if(node.lower_bound < upper_bound){
+				upper_bound = node.lower_bound;
+				best_s = node.subtours[0];
+			}
+			continue;	
+		}
+	
+		for(int i = 0; i < node.subtours[node.choosen].size() - 1; i++){
+			Node n;	
+			n.forbidden_arcs = node.forbidden_arcs;
+
+			pair<int, int> forbidden_arc;
+			//os subtours tem nodes a partir de 1
+			forbidden_arc.first = node.subtours[node.choosen][i] - 1;
+			forbidden_arc.second = node.subtours[node.choosen][i+1] - 1;
+
+			n.forbidden_arcs.push_back(forbidden_arc);
+			updateNode(&n, problem_info);
 			if(n.lower_bound <= upper_bound)
 				tree.push_back(n);	
 		}
 	}
 
-	for(auto& i : best_s)
-		cout << i << ", ";
-	cout << endl;
-
-
-	return 0;
 }
