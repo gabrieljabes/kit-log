51a1c8e9aab38c595236c8006e1c73b899143d32
Gabriel Jabes <gabriel.jabesldl@gmail.com>
2026-09-08 19:47:49 -0300

    corrigido a escolha de nós pela melhor aresta + iterações batem com o exemplo do kit

--- Branch-and-Cut/test_place/maxback.cpp
+++ Branch-and-Cut/test_place/maxback.cpp
@@ -11,7 +11,7 @@ std::vector<std::vector<double>> edges = {
     {1.0,  0.0, 0.25,  0.0, 0.75,  0.0,  0.0,  0.0},  // B
     {0.75, 0.25, 0.0,  1.0,  0.0,  0.0,  0.0,  0.0},  // C
     {0.25, 0.0,  1.0,  0.0, 0.75,  0.0,  0.0,  0.0},  // D
-    {0.0, 0.75,  0.0, 0.75,  0.0, 0.25, 0.25,  0.0},  // E
+    {0.0, 0.75,  0.0, 0.75,  0.0, 0.25, 0.25,  0.0},        // E
     {0.0,  0.0,  0.0,  0.0, 0.25,  0.0, 0.75,  1.0},  // F
     {0.0,  0.0,  0.0,  0.0, 0.25, 0.75,  0.0,  1.0},  // G
     {0.0,  0.0,  0.0,  0.0,  0.0,  1.0,  1.0,  0.0}   // H
@@ -21,47 +21,58 @@ std::vector<std::vector<double>> edges = {
     // vai testar com todos os nos
         int n = edges.size();
         vector<bool> S_group(n);
+
+        vector<double> w(n, 0);
         //0 -> i 
         int inicio = 0;
-        int fim;
-        S_group[inicio] = true;
+        double cut;
 
 
-        for(int i = 0; i < n - 1; i++){
+        S_group[0] = true;
+        int last_visited = 0;
+        for(int i = 0; i < n; i++){
+
+            int best_edge_index;
             double best_edge = 0;
-            int best_node_index = -1;
-            double cut = 0;
 
-            for(int j = 0; j < n; j++){
-                if(S_group[j] == true)
-                    continue;
-
-                if(edges[inicio][j] >= best_edge){
-                    best_edge = edges[inicio][j];
-                    best_node_index = j;
-                }
-            }
-
-            S_group[best_node_index] = true;
-            fim = best_node_index;
-
-            for(int j = 0; j < n ;j++){
+            cout << " ==== iteraçao " << i << endl;
+            for(int j = 0; j < w.size(); j++){
                 if(S_group[j])
                     continue;
-                for(int k = 0; k < n; k++){
-                    if(S_group[k])
-                        cut+= edges[j][k];
+                w[j] += edges[last_visited][j];
+                if(w[j] > best_edge && S_group[j] == false){
+                    best_edge = w[j];
+                    best_edge_index = j;
                 }
             }
-            
-            cout << "corte: " << cut << endl;
-            cout << "inicio: " << inicio << endl;
-            cout << "fim: " << fim << endl;
-            cout << endl;
+            double sum = 0;
+            for(int j = 0; j < w.size(); j++){
+                if(S_group[j])
+                    continue;
+                sum += w[j]; 
+            }
 
+            if(sum < 2){
+                cut = sum;
+                break;
+            } else{
+                S_group[best_edge_index] = true;
+                last_visited = best_edge_index;
+            }
+
+            cout << "w=" << endl;
+            for(auto &e : w)
+                cout << e << " ";
+            cout << endl << endl;
+
+
+        
+            cout << "soma=" << sum << endl;
         }
 
+        cout << "cut=" << cut << endl;
 
     return 0;
 
 }
+
